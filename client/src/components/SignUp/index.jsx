91e849103748e4500cfba3e9b684d24ae3184ab2
Dante Ramón Gil-Marín <drgilmarin@uchicago.edu>
2021-05-14 22:50:13 -0500

    added css to sign up page

--- client/src/components/SignUp/index.jsx
+++ client/src/components/SignUp/index.jsx
@@ -1,16 +1,17 @@
 import React, { Component } from "react";
-import { Link, withRouter } from "react-router-dom";
+import { withRouter } from "react-router-dom";
 import { compose } from "recompose";
 
 import { withFirebase } from "../Firebase";
 
 import * as ROUTES from "../../constants/routes";
 
+import logo from "../logo.svg";
+
 import { wishlistApi, closetApi, userApi } from "../../api";
 
 const SignUpPage = () => (
   <div>
-    <h1>SignUp</h1>
     <SignUpForm />
   </div>
 );
@@ -60,37 +61,75 @@ class SignUpFormBase extends Component {
   render() {
     const { username, email, password, error } = this.state;
 
-    const isInvalid = password === "" || email === "" || username === "";
-
     return (
-      <form onSubmit={this.onSubmit}>
-        <input
-          name="username"
-          value={username}
-          onChange={this.onChange}
-          type="text"
-          placeholder="Username"
-        />
-        <input
-          name="email"
-          value={email}
-          onChange={this.onChange}
-          type="text"
-          placeholder="Email Address"
-        />
-        <input
-          name="password"
-          value={password}
-          onChange={this.onChange}
-          type="password"
-          placeholder="Password"
-        />
-        <button disabled={isInvalid} type="submit">
-          Sign Up
-        </button>
+      <div className="min-h-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
+        <div className="max-w-md w-full space-y-8">
+          <div>
+            <img src={logo} className="mx-auto h-12 w-auto" alt="cloth-logo" />
+            <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">Sign up </h2>
+          </div>
+          <form className="mt-8 space-y-6" action="#" method="POST">
+            <input type="hidden" name="remember" defaultValue="true" />
+            <div className="rounded-md shadow-sm -space-y-px">
+              <div>
+                <label htmlFor="email-address" className="sr-only">
+                  Username
+                </label>
+                <input
+                  name="username"
+                  value={username}
+                  onChange={this.onChange}
+                  type="text"
+                  required
+                  className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                  placeholder="Username"
+                />
+              </div>
+              <div>
+                <label htmlFor="email-address" className="sr-only">
+                  Email address
+                </label>
+                <input
+                  name="email"
+                  value={email}
+                  onChange={this.onChange}
+                  type="email"
+                  autoComplete="email"
+                  required
+                  className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                  placeholder="Email Address"
+                />
+              </div>
+              <div>
+                <label htmlFor="password" className="sr-only">
+                  Password
+                </label>
+                <input
+                  name="password"
+                  value={password}
+                  onChange={this.onChange}
+                  type="password"
+                  autoComplete="current-password"
+                  required
+                  className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                  placeholder="Password"
+                />
+              </div>
+            </div>
 
-        {error && <p>{error.message}</p>}
-      </form>
+            <div>
+              <button
+                type="submit"
+                onClick={this.onSubmit}
+                className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+              >
+                Sign up
+              </button>
+            </div>
+          </form>
+          {error && <p>{error.message}</p>}
+        </div>
+      </div>
     );
   }
 }
