881f5a670a77895cf92beae3608ee32127120052
Dante Ramón Gil-Marín <drgilmarin@uchicago.edu>
2021-05-15 03:52:22 -0500

    cahnged navbar usr img

--- client/src/components/Navigation/NavBar.jsx
+++ client/src/components/Navigation/NavBar.jsx
@@ -9,7 +9,7 @@ import * as ROUTES from "../../constants/routes";
 
 import { Fragment } from 'react'
 import { Disclosure, Menu, Transition } from '@headlessui/react'
-import { MenuIcon, XIcon } from '@heroicons/react/outline'
+import { MenuIcon, XIcon, User } from '@heroicons/react/outline'
 
 function classNames(...classes) {
   return classes.filter(Boolean).join(' ')
@@ -150,11 +150,7 @@ class NavigationAuth extends Component {
                         <div>
                           <Menu.Button className="bg-gray-800 flex text-sm rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
                             <span className="sr-only">Open user menu</span>
-                            <img
-                              className="h-8 w-8 rounded-full"
-                              src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                              alt=""
-                            />
+                            <User className="h-8 w-8 rounded-full" aria-hidden="true" />
                           </Menu.Button>
                         </div>
                         <Transition
