5d3ce0f209c23fdce56add7e4b60c4bbee11a0a8
Dante Ramón Gil-Marín <drgilmarin@uchicago.edu>
2021-05-14 22:56:41 -0500

    added css to password forget

--- client/src/components/PasswordForget/index.jsx
+++ client/src/components/PasswordForget/index.jsx
@@ -1,9 +1,10 @@
 import React, { Component } from "react";
 import { withFirebase } from "../Firebase";
 
+import logo from "../logo.svg";
+
 const PasswordForgetPage = () => (
   <div>
-    <h1>PasswordForget</h1>
     <PasswordForgetForm />
   </div>
 );
@@ -42,23 +43,47 @@ class PasswordForgetFormBase extends Component {
   render() {
     const { email, error } = this.state;
 
-    const isInvalid = email === "";
-
     return (
-      <form onSubmit={this.onSubmit}>
-        <input
-          name="email"
-          value={this.state.email}
-          onChange={this.onChange}
-          type="text"
-          placeholder="Email Address"
-        />
-        <button disabled={isInvalid} type="submit">
-          Reset My Password
-        </button>
+      <div className="min-h-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
+        <div className="max-w-md w-full space-y-8">
+          <div>
+            <img src={logo} className="mx-auto h-12 w-auto" alt="cloth-logo" />
+            <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900"> Reset Password </h2>
+          </div>
+          <form className="mt-8 space-y-6" action="#" method="POST">
+            <input type="hidden" name="remember" defaultValue="true" />
+            <div className="rounded-md shadow-sm -space-y-px">
+              <div>
+                <label htmlFor="email-address" className="sr-only">
+                  Email address
+                </label>
+                <input
+                  name="email"
+                  value={email}
+                  onChange={this.onChange}
+                  type="email"
+                  autoComplete="email"
+                  required
+                  className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                  placeholder="Email Address"
+                />
+              </div>
+            </div>
 
-        {error && <p>{error.message}</p>}
-      </form>
+            <div>
+              <button
+                type="submit"
+                onClick={this.onSubmit}
+                className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+              >
+                Reset My Password
+              </button>
+            </div>
+          </form>
+          {error && <p>{error.message}</p>}
+        </div>
+      </div>
+
     );
   }
 }
