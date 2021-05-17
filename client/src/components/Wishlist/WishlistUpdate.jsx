5f1deff0a17fb576340362b7b6dceb622c357aa5
Dante Ramón Gil-Marín <drgilmarin@uchicago.edu>
2021-05-17 18:19:20 -0500

    added css to mywishlist

--- client/src/components/Wishlist/WishlistUpdate.jsx
+++ client/src/components/Wishlist/WishlistUpdate.jsx
@@ -19,15 +19,23 @@ class LinkItem extends Component {
 
   render() {
     return (
-      <div>
-        <div>Type: {this.props.item.type}</div>
-        <div>Brand: {this.props.item.brand}</div>
-        <div>Season: {this.props.item.season}</div>
-        <div>
-          <button onClick={this.itemLink}>GO</button>
+      <div className="m-10 flex">
+        <div className="w-40 h-40 overflow-hidden rounded shadow-lg">
+          <img src={this.props.item.imageURL} alt="product" />
         </div>
-        <div>
-          <button onClick={this.removeItem}>Remove</button>
+        <div className="flex-wrap p-5">
+          <h1 className="flex-auto text-xl font-semibold">
+            Short Description
+          </h1>
+          <div className="w-full text-sm font-medium text-gray-500 mt-2">
+            Brand: {this.props.item.brand}
+          </div>
+          <div>
+            <button className="font-medium text-indigo-600 hover:text-indigo-500" onClick={this.itemLink}>GO</button>
+          </div>
+          <div>
+            <button className="font-medium text-red-600 hover:text-red-500" onClick={this.removeItem}>Remove</button>
+          </div>
         </div>
       </div>
     );
@@ -75,9 +83,13 @@ class WishlistUpdate extends Component {
     console.log("TCL: ItemsGrid -> render -> items", items);
 
     return (
-      <div>
-        <h1>My Wishlist</h1>
-        <div className="grid grid-cols-1">{listItems}</div>
+      <div className="bg-white shadow overflow-hidden sm:rounded-lg">
+        <div className="px-4 py-5 sm:px-6">
+          <h3 className="text-lg leading-6 font-medium text-gray-900">My Wishlist</h3>
+        </div>
+        <div className="border-t border-gray-200">
+          {listItems}
+        </div>
       </div>
     );
   }
